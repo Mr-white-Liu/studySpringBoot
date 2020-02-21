4143cec082c51c621af8c15e63999c717255892b
Mr-white-Liu <2335213424@qq.com>
2020-02-21 20:52:24 +0800

    p15 finish

--- src/main/java/com/example/study/controller/AuthorizeController.java
+++ src/main/java/com/example/study/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -25,7 +27,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                           HttpServletRequest httpServletRequest){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -35,8 +38,10 @@ public class AuthorizeController {
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = gitHubProvider.getUser(accessToken);
 
-        System.out.println(user.getName());
+        if(user != null){
+            httpServletRequest.getSession().setAttribute("user",user);
+        }
 
-        return "index";
+        return "redirect:/";
     }
 }
