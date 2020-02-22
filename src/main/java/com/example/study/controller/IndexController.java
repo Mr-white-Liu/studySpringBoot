dda0100485b9e91c3ddabf9cb302f8a86c430cb5
Mr-white-Liu <2335213424@qq.com>
2020-02-22 18:50:18 +0800

    p18 finished

--- src/main/java/com/example/study/controller/IndexController.java
+++ src/main/java/com/example/study/controller/IndexController.java
@@ -1,14 +1,35 @@
 package com.example.study.controller;
 
+import com.example.study.mapper.UserMapper;
+import com.example.study.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String hello() {
+    public String index(HttpServletRequest httpServletRequest) {
+        Cookie[] cookies = httpServletRequest.getCookies();
+        if(cookies!=null)
+            for (Cookie cookie : cookies) {
+                if(cookie.getName().equals("token")){
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+
+                    if (user!=null){
+                        httpServletRequest.getSession().setAttribute("user",user);
+                    }
+                    break;
+                }
+            }
         return "index";
     }
 }
--- src/main/java/com/example/study/mapper/UserMapper.java
+++ src/main/java/com/example/study/mapper/UserMapper.java
@@ -3,11 +3,14 @@ package com.example.study.mapper;
 import com.example.study.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
     @Insert("insert into user (name,account_id,token,gmt_creat,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreat},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(String token);
 }
